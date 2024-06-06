93c4b68958f1d724d3935906a1293f75866099a1
mars <youredith3@gmail.com>
2024-06-06 22:50:06 +0900

    add: shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
           <div className="mockup-phone">
             <div className="camera"></div>
             <div className="display ">
-              <div className="artboard artboard-demo phone-4 bg-base-100">
-                <div className="relative w-full h-[780px] pt-10 flex-col items-center bg-base-100">
+              <div className="artboard artboard-demo phone-4 bg-white ">
+                <div className=" w-full relative  h-[780px] flex-col items-center bg-base-100">
                   {children}
                 </div>
               </div>
